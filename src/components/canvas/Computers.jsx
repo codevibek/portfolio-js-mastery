e9273c0e03c6431c7b690b74aba224a7ce48f08e
vibek lamichhane <vibek@copenned.com>
2023-05-17 12:14:36 +0545

    add animations to the about section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,19 +4,34 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
 import CanvasLoader from "../Loader"
  
-const Computers = () => {
+const Computers = ({isMobile}) => {
   const computer = useGLTF("./desktop_pc/scene.gltf")
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={0.5}/>
       <spotLight position={[-20,50,10]} angle={0.22} penumbra={1} intensity={1} castShadow shadow-mapSize={1024}/>
-      <primitive object={computer.scene} scale={0.75} position={[0,-3.5,-3.5]} rotation={[-0.01,-0.2,-0.1]}/>
+      <primitive object={computer.scene} scale={isMobile?0.4:0.75} position={isMobile?[-0.01,-2.5,-0.7]:[0,-3.5,-3.5]} rotation={[-0.01,-0.2,-0.1]}/>
     </mesh>
   )
 }
 
 const ComputersCanvas = ()=>{
+
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(()=>{
+    const mediaQuery=window.matchMedia("(max-width:500px)");
+    setIsMobile(mediaQuery.matches)
+
+    const handleMediaQueryChange = (event)=>{ 
+      setIsMobile(event.matches)
+    }
+
+    mediaQuery.addEventListener("change",handleMediaQueryChange)
+   return ()=> {mediaQuery.removeEventListener("change",handleMediaQueryChange)}
+  },[])
+
   return(
     <Canvas
     frameloop='demand'
@@ -28,7 +43,7 @@ const ComputersCanvas = ()=>{
         maxPolarAngle={Math.PI/2}
         minPolarAngle={Math.PI/2}
         enableZoom = {false}/>
-        <Computers/>
+        <Computers isMobile={isMobile}/>
       </Suspense>
     </Canvas>
   )
