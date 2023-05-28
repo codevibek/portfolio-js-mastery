8d9c968d663c5de0eafa19202a03576b8f5166d2
vibek lamichhane <vibek@copenned.com>
2023-05-28 19:52:12 +0545

    create globe in contact

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,8 +17,7 @@ const Navbar = () => {
         }}>
           <img src={fulllogo} alt="logo" className="w-9 h-9 object-contain"/>
           <p className="text-white text-[18px] font-bold cursor-pointer">
-            Vibek 
-            <span className="sm:block hidden">Lamichhane</span></p>
+            Vibek <span className="sm:block hidden">Lamichhane</span></p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link)=>(
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React,{Suspense} from 'react'
+import {Canvas} from "@react-three/fiber"
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+
+import CanvasLoader from "../Loader"
 
 const Earth = () => {
+
+  const earth = useGLTF("./planet/scene.gltf")
+
   return (
-    <div>Earth</div>
+   
+    <primitive 
+    object={earth.scene}
+    scale={1.5}
+    position-y={0}
+    rotation-y={0}
+    
+    />
   )
 }
 
-export default Earth
+
+const EarthCanvas=()=>{
+return(
+  <Canvas
+  frameloop='demand'
+  shadows
+  gl={{preserveDrawingBuffer:true}}
+  camera={{position:[-4,3,6], fov:25,near:0.1,far:100}}>
+    <Suspense fallback = {<CanvasLoader/>}>
+      <OrbitControls 
+      autoRotate
+      maxPolarAngle={Math.PI/2}
+      minPolarAngle={Math.PI/2}
+      enableZoom = {false}/>
+      <Earth />
+    </Suspense>
+  </Canvas>
+)
+}
+export default EarthCanvas
